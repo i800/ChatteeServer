6cf0c7f32abd29bd339cceb5b77a5fe8e0d801ca
Cyril Usatschoff <usatschoff@gmail.com>
2018-05-15 20:24:05 +0300

    DB connected

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <QCoreApplication>
+#include "Server.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    Server server;
+    server.start();
     return a.exec();
 }
