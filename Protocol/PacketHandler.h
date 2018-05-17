41988bc892af5c62d3aaadd2258b9c39924309e6
Cyril Usatschoff <usatschoff@gmail.com>
2018-05-17 17:32:33 +0300

    PacketHandler interface added

--- Protocol/PacketHandler.h
+++ Protocol/PacketHandler.h
@@ -1,6 +1,6 @@
 #ifndef _PACKET_HANDLER_
 #define _PACKET_HANDLER_
-#include <QObject>
+#include "Protocol/Packet.h"
 
 class PacketHandler : public QObject
 {
@@ -9,7 +9,20 @@ private:
     PacketHandler(const PacketHandler&) = delete;
     PacketHandler& operator=(const PacketHandler&) = delete;
 public:
-
+    // Packet id equals to index in the enum.
+    enum PacketMeta
+    {
+        USER_REG = 1,
+        USER_LOG = 2,
+        USER_OUT = 3,
+        USER_ADD_CHAT = 4,
+        USER_ADD_MESS = 5
+    };
+    PacketHandler();
+    ~PacketHandler();
+    const bool isValidPacket(const QByteArray&);
+    const Packet* makePacket(const QByteArray&);
+    const PacketMeta& getPacketMeta(const ByteArray&);
 };
 
 #endif
